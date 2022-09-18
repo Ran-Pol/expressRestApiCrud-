fea7009602a1f210fe5618267fbaf87b32a91da9
Ran Pol <rnd809@gmail.com>
2022-09-18 05:59:53 -0400

    REST API CRUD: Establish the basic HTTP methods Get, Post, Get/:id

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ import express from "express";
 import morgan from "morgan";
 
 const app = express();
+const products = [{ id: 1, name: "laptop", price: 300 }];
 
 app.use(morgan("dev"));
+app.use(express.json());
 
 app.get("/products", (req, res) => {
-  res.send("Obtaining products");
+  res.send(products);
 });
 
 app.post("/products", (req, res) => {
-  res.send("Creating products");
+  const newProduct = { ...req.body, id: products.length + 1 };
+  products.push(newProduct);
+
+  res.send(newProduct);
 });
 
 app.put("/products", (req, res) => {
@@ -22,7 +27,16 @@ app.delete("/products", (req, res) => {
 });
 
 app.get("/products/:id", (req, res) => {
-  res.send("Obtaining product");
+  console.log(req.params.id);
+  const productRequested = products.find((item) => item.id === +req.params.id);
+
+  if (!productRequested)
+    return res.status(404).json({
+      message: "Product not found.",
+    });
+
+  console.log(productRequested);
+  res.json(productRequested);
 });
 
 app.listen(3000);
